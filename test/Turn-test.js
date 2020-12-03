80123b7c1814eb1f2622d8e612b6cd68a3c3d685
Richard Tyler <richardtyler2011@gmail.com>
2020-12-03 15:45:07 -0700

    Refactor indentation, readability of objects, and include beforeEach

--- test/Turn-test.js
+++ test/Turn-test.js
@@ -5,54 +5,53 @@ const Card = require('../src/Card');
 const Turn = require('../src/Turn');
 
 describe('Turn', function() {
-  let 
 
-    it('should be a function', function() {
-      const turn = new Turn();
-      expect(Turn).to.be.a('function');
+  let turn, card;
+
+  beforeEach(function() {
+    const card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
+    const turn = new Turn('object', card);
+  });
+
+  it('should be a function', function() {
+    expect(Turn).to.be.a('function');
+  });
+
+  it('should be an instance of Turn', function() {
+    expect(turn).to.be.an.instanceOf(Turn);
+  });
+
+  it('should store a guess', function() {
+    expect(turn.guess).to.deep.equal('object');
+  });
+
+  it('should store a card', function(){
+    expect(turn.card).to.deep.equal({
+      id: 1, 
+      question: 'What allows you to define a set of related information using key-value pairs?', 
+      answers: ['object', 'array', 'function'], 
+      correctAnswer: 'object'
     });
+  });
 
-    it('should be an instance of Turn', function() {
-      const turn = new Turn();
-      expect(turn).to.be.an.instanceOf(Turn);
+  it('should return the user\'s guess', function() {
+    expect(turn.returnGuess()).to.deep.equal('object');
+  });
+
+  it('should return the user\'s card', function() {
+    expect(turn.returnCard()).to.deep.equal({
+      id: 1, 
+      question: 'What allows you to define a set of related information using key-value pairs?', 
+      answers: ['object', 'array', 'function'], 
+      correctAnswer: 'object'
     });
+  });
 
-    it('should store a guess', function() {
-      const card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
-      const turn = new Turn('object');
-      expect(turn.guess).to.deep.equal('object');
-    });
+  it('should evaluate the user\'s guess', function() {
+    expect(turn.evaluateGuess()).to.deep.equal(true);
+  });
 
-    it('should store a card', function(){
-      const card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
-      const turn = new Turn('object', card);
-      expect(turn.card).to.deep.equal({id: 1, question: 'What allows you to define a set of related information using key-value pairs?', answers: ['object', 'array', 'function'], correctAnswer: 'object'});
-    });
-
-    it('should return the user\'s guess', function() {
-      const card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
-      const turn = new Turn('object', card);
-      expect(turn.returnGuess()).to.deep.equal('object');
-    });
-
-    it('should return the user\'s card', function() {
-      const card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
-      const turn = new Turn('object', card);
-      expect(turn.returnCard()).to.deep.equal({id: 1, question: 'What allows you to define a set of related information using key-value pairs?', answers: ['object', 'array', 'function'], correctAnswer: 'object'});
-    });
-
-    it('should evaluate the user\'s guess', function() {
-      const card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
-      const turn = new Turn('object', card);
-      expect(turn.evaluateGuess()).to.deep.equal(true);
-    });
-
-    it('should give feedback on the user\'s guess', function() {
-      const card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
-      const turn = new Turn('object', card);
-      expect(turn.giveFeedback()).to.deep.equal('correct!');
-    });
-});{
-
-  
-}
+  it('should give feedback on the user\'s guess', function() {
+    expect(turn.giveFeedback()).to.deep.equal('correct!');
+  });
+});
