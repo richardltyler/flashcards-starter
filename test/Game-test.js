b6dbc01e93c27d346da498d93b323dac9152bcc8
Richard Tyler <richardtyler2011@gmail.com>
2020-12-03 15:30:19 -0700

    Remove unused variables

--- test/Game-test.js
+++ test/Game-test.js
@@ -1,8 +1,6 @@
 const chai = require('chai');
 const expect = chai.expect;
 
-const Card = require('../src/Card');
-const Deck = require('../src/Deck');
 const Game = require('../src/Game');
 const Round = require('../src/Round');
 
